04e8c34f2d1c62deb46915f74f23e374952e3900
jadri138 <57437206+jadri138@users.noreply.github.com>
2025-11-13 17:26:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json(message: 'Esta sera la proxima web de Ruralicos! 🚜);
+  res.json(message: 'Esta sera la proxima web de Ruralicos!🚜);
 });
 
 const PORT = process.env.PORT || 3000;
