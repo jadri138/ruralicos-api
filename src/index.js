61a18d26c796380c6142d34c0d3200ad78a1c293
jadri138 <xeroxgamer420@gmail.com>
2025-11-25 23:54:37 +0100

    actualizacion 1.0 + web

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const { supabase } = require('./supabaseClient');
 const enviarWhatsapp = require('./whatsapp');
 
