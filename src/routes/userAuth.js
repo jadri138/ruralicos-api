d51923b0b9d9c3d58f69aa4ca064e2e97f9d087c
jadri138 <xeroxgamer420@gmail.com>
2025-12-02 18:35:50 +0100

    login usuarios

--- src/routes/userAuth.js
+++ src/routes/userAuth.js
@@ -1,7 +1,7 @@
 // src/routes/userAuth.js
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { requireAuth } = require('../../authMiddleware');
+const authMiddleware = require('../../authMiddleware');
 
 
 module.exports = (app, supabase) => {
