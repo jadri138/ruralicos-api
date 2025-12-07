77c64cb8089facf2531f9af6fe4614469e28610b
jadri138 <xeroxgamer420@gmail.com>
2025-12-07 09:13:37 +0100

    scraper boa

--- src/boletines/scrapeTest.js
+++
@@ -1,8 +0,0 @@
-const { scrapeBoaHoy } = require('./boaScraper');
-
-(async () => {
-  const data = await scrapeBoaHoy();
-
-  console.log('ENCONTRADOS HOY:', data.length);
-  console.log(data.slice(0, 10)); // ver los 10 primeros
-})();
